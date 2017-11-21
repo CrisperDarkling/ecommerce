4886256ae69fdb58c67b15017e46dd4717061574
Yazeed Kelly <yazeedkelly@hotmail.com>
2017-11-21 16:52:29 +0000

    cart done

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -13,12 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from products.views import get_index, all_products
+from cart import urls as cart_urls
+from .settings import MEDIA_ROOT
+from django.views import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r"^$", all_products, name="products"),
+    url(r"^$", all_products, name="home"),
+    url(r"^cart/", include(cart_urls)),
+    url(r'^media/(?P<path>.*)$', static.serve,{'document_root': MEDIA_ROOT}),
     
 ]
